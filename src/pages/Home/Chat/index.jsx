6dae6b4c7e69e8d2117337c234e690032354dddd
Heath <duweihengmr@163.com>
2024-05-10 20:07:56 +0800

    chat page

--- src/pages/Home/Chat/index.jsx
+++ src/pages/Home/Chat/index.jsx
@@ -4,10 +4,14 @@ import {useState} from "react";
 import Time from "./ChatContent/Time/index.jsx";
 import Msg from "./ChatContent/Msg/index.jsx";
 import CustomButton from "../../../componets/CustomButton/index.jsx";
+import RightClickMenu from "../../../componets/RightClickMenu/index.jsx";
+import IconMinorButton from "../../../componets/IconMinorButton/index.jsx";
 
 export default function Chat() {
 
     const [selectedChatId, setSelectedChatId] = useState("1")
+    const [menuPosition, setMenuPosition] = useState(null);
+    const [menuVisible, setMenuVisible] = useState(null);
 
     const topChatsData = [
         {
@@ -32,14 +36,70 @@ export default function Chat() {
             lastMsgTime: "14:20",
             unreadNum: 10
         }
+        ,
+        {
+            id: "8",
+            remark: "小蓝小蓝小蓝小蓝小蓝小蓝小蓝小蓝小蓝",
+            lastMsg: "你在干什么?你在干什么?你在干什么?你在干什么?你在干什么?",
+            lastMsgTime: "14:20",
+            unreadNum: 10
+        },
+        {
+            id: "9",
+            remark: "小蓝",
+            lastMsg: "你在干什么?",
+            lastMsgTime: "14:20",
+            unreadNum: 10
+        },
+        {
+            id: "4",
+            remark: "小蓝",
+            lastMsg: "你在干什么?",
+            lastMsgTime: "14:20",
+            unreadNum: 10
+        },
+        {
+            id: "5",
+            remark: "小蓝",
+            lastMsg: "你在干什么?",
+            lastMsgTime: "14:20",
+            unreadNum: 10
+        },
+        {
+            id: "6",
+            remark: "小蓝",
+            lastMsg: "你在干什么?",
+            lastMsgTime: "14:20",
+            unreadNum: 10
+        },
+        {
+            id: "7",
+            remark: "小蓝",
+            lastMsg: "你在干什么?",
+            lastMsgTime: "14:20",
+            unreadNum: 10
+        }
     ]
 
-    const ChatCard = ({info, onClick}) => {
+    const chatListRightOptions = [
+        {key: "top", label: "置顶"},
+        {key: "unTop", label: "取消置顶"},
+        {key: "unNoDisturb", label: "设置免打扰"},
+        {key: "unNoDisturb ", label: "取消免打扰"},
+        {key: "newWindow", label: "打开独立窗口"},
+        {key: "deleteChat", label: "从聊天列表中移除"},
+    ]
+
+    const ChatCard = ({info, onClick, onContextMenu}) => {
         let isSelected = info.id === selectedChatId
         return (
             <div
                 className={`chat-card ${isSelected ? "selected" : ""}`}
                 onClick={() => onClick(info)}
+                onContextMenu={(e) => {
+                    e.preventDefault()
+                    if (onContextMenu) onContextMenu(e)
+                }}
             >
                 <div className="chat-card-portrait"></div>
                 <div className="chat-card-content">
@@ -50,6 +110,7 @@ export default function Chat() {
                                 fontWeight: 600,
                                 color: `${isSelected ? "#FFF" : "1F1F1F"}`
                             }}
+                            className="ellipsis"
                         >
                             {info.remark ? info.remark : info.name}</div>
                         <div style={{
@@ -58,10 +119,16 @@ export default function Chat() {
                         }}>{info.lastMsgTime}</div>
                     </div>
                     <div className="chat-card-content-item">
-                        <div style={{fontSize: 12, color: `${isSelected ? "#F6F6F6" : "#646464"}`}}>{info.lastMsg}</div>
+                        <div
+                            style={{fontSize: 12, color: `${isSelected ? "#F6F6F6" : "#646464"}`}}
+                            className="ellipsis"
+                        >
+                            {info.lastMsg}
+                        </div>
                         {
                             info.unreadNum > 0 ? <div style={{
                                     width: 18,
+                                    minWidth: 18,
                                     height: 18,
                                     borderRadius: 16,
                                     backgroundColor: "#4C9BFF",
@@ -90,18 +157,36 @@ export default function Chat() {
                         <CustomSearchInput></CustomSearchInput>
                     </div>
                 </div>
-                <div className="chat-list-items">
+                <RightClickMenu visible={menuVisible} position={menuPosition} options={chatListRightOptions}/>
+                <div
+                    onScroll={e => {
+                        setMenuVisible({visible: false})
+                    }}
+                    className="chat-list-items">
                     <div className="chat-list-items-label">置顶</div>
                     {
                         topChatsData.map(data => {
-                            return <ChatCard info={data} onClick={() => setSelectedChatId(data.id)}/>
+                            return (
+                                <ChatCard
+                                    onContextMenu={(e) => setMenuPosition({x: e.clientX, y: e.clientY})}
+                                    info={data}
+                                    onClick={() => setSelectedChatId(data.id)}
+                                />
+                            )
                         })
                     }
                     <div className="chat-list-items-label">全部</div>
                     {
                         allChatsData.map(data => {
-                            return <ChatCard info={data} onClick={() => setSelectedChatId(data.id)}/>
+                            return (
+                                <ChatCard
+                                    onContextMenu={(e) => setMenuPosition({x: e.clientX, y: e.clientY})}
+                                    info={data}
+                                    onClick={() => setSelectedChatId(data.id)}
+                                />
+                            )
                         })
+
                     }
                 </div>
             </div>
@@ -134,13 +219,17 @@ export default function Chat() {
                 <div className="chat-content-send-frame">
                     <div className="chat-content-send-frame-operation">
                         <div style={{display: "flex"}}>
-                            <i className={`iconfont icon icon-biaoqing`} style={{fontSize: 24}}/>
-                            <i className={`iconfont icon icon-wenjian`} style={{fontSize: 26}}/>
-                            <i className={`iconfont icon icon-jilu`} style={{fontSize: 22}}/>
+                            <IconMinorButton
+                                icon={<i className={`iconfont icon icon-biaoqing`} style={{fontSize: 24}}/>}/>
+                            <IconMinorButton
+                                icon={<i className={`iconfont icon icon-wenjian`} style={{fontSize: 26}}/>}/>
+                            <IconMinorButton icon={<i className={`iconfont icon icon-jilu`} style={{fontSize: 22}}/>}/>
                         </div>
                         <div style={{display: "flex"}}>
-                            <i className={`iconfont icon icon-dianhua`} style={{fontSize: 24}}/>
-                            <i className={`iconfont icon icon-shipin`} style={{fontSize: 26}}/>
+                            <IconMinorButton
+                                icon={<i className={`iconfont icon icon-dianhua`} style={{fontSize: 24}}/>}/>
+                            <IconMinorButton
+                                icon={<i className={`iconfont icon icon-shipin`} style={{fontSize: 26}}/>}/>
                         </div>
                     </div>
                     <div className="chat-content-send-frame-msg">
