d712df494ccfd7a489cc19284317d09b6d92bb41
Heath <duweihengmr@163.com>
2024-05-12 18:56:58 +0800

    CustomOverlay

---
+++ src/componets/CustomOverlay/index.jsx
@@ -0,0 +1,35 @@
+import "./index.less"
+import {useEffect, useRef, useState} from "react";
+
+export default function CustomOverlay({visible, position, width, children}) {
+    const [inVisible, setInVisible] = useState(visible)
+    const contentRef = useRef(null);
+
+    useEffect(() => {
+        setInVisible(visible)
+    }, [visible])
+
+    useEffect(() => {
+        if (contentRef.current) {
+            contentRef.current.focus()
+        }
+    }, [inVisible])
+
+    return (
+        <div>
+            <div className="custom-overlay">
+                {inVisible &&
+                    <div
+                        ref={contentRef}
+                        className="content"
+                        style={{top: position.x, left: position.y, width: width, outline: "none"}}
+                        tabIndex="0"
+                        onBlur={() => setInVisible(false)}
+                    >
+                        {children}
+                    </div>
+                }
+            </div>
+        </div>
+    )
+}
