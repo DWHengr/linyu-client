b54c488889372f8fbfefe9eff579962572570c98
Heath <duweihengmr@163.com>
2024-05-13 18:55:15 +0800

    App

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home/index.jsx";
 import Login from "./pages/Login/index.jsx";
 import TrayMenu from "./pages/TrayMenu/index.jsx";
 import CreateTrayWindow from "./pages/TrayMenu/window.jsx";
+import ChatWindow from "./pages/ChatWindow/index.jsx";
 
 CreateTrayWindow()
 function App() {
@@ -22,6 +23,7 @@ function App() {
                 <Route path="/login" component={Login}></Route>
                 <Route path="/home" component={Home}></Route>
                 <Route path="/tray" component={TrayMenu}></Route>
+                <Route path="/chat" component={ChatWindow}></Route>
                 <Redirect path="/" to="/login"/>
             </Switch>
         </div>
