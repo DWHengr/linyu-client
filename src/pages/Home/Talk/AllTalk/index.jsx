15ca9841d15ae69dab8e349f3d72c431894aaa37
Heath <duweihengmr@163.com>
2024-05-14 20:04:49 +0800

    AllTalk

---
+++ src/pages/Home/Talk/AllTalk/index.jsx
@@ -0,0 +1,184 @@
+import "./index.less"
+import CustomButton from "../../../../componets/CustomButton/index.jsx";
+import CustomTextarea from "../../../../componets/CustomTextarea/index.jsx";
+import {useHistory} from "react-router-dom";
+
+export default function AllTalk() {
+    let h = useHistory();
+
+    let talkAllData = [
+        {
+            user: {
+                portrait: "",
+                name: "小红"
+            },
+            content: {
+                describe: "又是疲劳的一天",
+                media: [
+                    {
+                        type: "img",
+                        mediaContent: "https://th.bing.com/th/id/OIP.kt5NiXbTpov01ELs6cs8tQHaEo?rs=1&pid=ImgDetMain"
+                    }
+                ],
+            },
+            time: "20分钟前",
+            like: {
+                num: 10,
+                likeContent: []
+            },
+            comment: {
+                num: 10,
+                commentContent: [
+                    "小蓝：今天怎么了",
+                    "小蓝：今天工作很忙么？"
+                ]
+            }
+        },
+        {
+            user: {
+                portrait: "",
+                name: "小红"
+            },
+            content: {
+                describe: "又是疲劳的一天",
+                media: [
+                    {
+                        type: "img",
+                        mediaContent: "https://th.bing.com/th/id/OIP.kt5NiXbTpov01ELs6cs8tQHaEo?rs=1&pid=ImgDetMain"
+                    }
+                ],
+            },
+            time: "",
+            like: {
+                num: 10,
+                likeContent: []
+            },
+            comment: {
+                num: 10,
+                commentContent: [
+                    "小蓝：今天怎么了",
+                    "小蓝：今天工作很忙么？"
+                ]
+            }
+        },
+        {
+            user: {
+                portrait: "",
+                name: "小红"
+            },
+            content: {
+                describe: "又是疲劳的一天",
+                media: [
+                    {
+                        type: "img",
+                        mediaContent: "https://th.bing.com/th/id/OIP.kt5NiXbTpov01ELs6cs8tQHaEo?rs=1&pid=ImgDetMain"
+                    }
+                ],
+            },
+            time: "",
+            like: {
+                num: 10,
+                likeContent: []
+            },
+            comment: {
+                num: 10,
+                commentContent: [
+                    "小蓝：今天怎么了",
+                    "小蓝：今天工作很忙么？"
+                ]
+            }
+        },
+        {
+            user: {
+                portrait: "",
+                name: "小红"
+            },
+            content: {
+                describe: "又是疲劳的一天",
+                media: [
+                    {
+                        type: "img",
+                        mediaContent: "https://th.bing.com/th/id/OIP.kt5NiXbTpov01ELs6cs8tQHaEo?rs=1&pid=ImgDetMain"
+                    }
+                ],
+            },
+            time: "",
+            like: {
+                num: 10,
+                likeContent: []
+            },
+            comment: {
+                num: 10,
+                commentContent: [
+                    "小蓝：今天怎么了",
+                    "小蓝：今天工作很忙么？"
+                ]
+            }
+        }
+    ]
+
+
+    return (
+        <div data-tauri-drag-region className="all-talk-container">
+            <div style={{position: "absolute", top: 20, left: 10}}>
+                <CustomButton onClick={{}}>说一说</CustomButton>
+            </div>
+            <div className="talks">
+                {talkAllData?.map((item) => {
+                    return (
+                        <div
+                            className="talk"
+                            onClick={() => h.push("/home/talk/detail")}
+                        >
+                            <div className="talk-title">
+                                <div className="talk-title-portrait"></div>
+                                <div className="talk-title-info">
+                                    <div className="talk-title-info-name">{item.user.name}</div>
+                                    <div className="talk-title-info-time">{item.time}</div>
+                                </div>
+                            </div>
+                            <div className="talk-content">
+                                <div>{item.content.describe}</div>
+                                {item.content.media?.map((mediaItem) => {
+                                    return (
+                                        <img
+                                            className="talk-content-img"
+                                            src={mediaItem.mediaContent}
+                                        />
+                                    )
+                                })}
+                            </div>
+                            <div className="talk-bottom">
+                                <div className="talk-bottom-operation">
+                                    <div className="talk-bottom-operation-item">
+                                        <i className={`iconfont icon-star`}
+                                           style={{fontSize: 14, marginRight: 4, color: "#4C9BFF"}}/>
+                                        <div>已点赞（{item.like.num}）</div>
+                                    </div>
+                                    <div className="talk-bottom-operation-item">
+                                        <div>评论（{item.comment.num}）</div>
+                                    </div>
+                                </div>
+                                <CustomTextarea>
+                                    <div style={{display: "flex", alignItems: "end", justifyContent: "end", margin: 5}}>
+                                        <CustomButton width={60}>发送</CustomButton>
+                                    </div>
+                                </CustomTextarea>
+                                <div>
+                                    {item.comment.commentContent?.map((commentItem) => {
+                                        return <div>
+                                            {commentItem}
+                                        </div>
+                                    })}
+                                </div>
+                                <div style={{color: "#4C9BFF"}}>
+                                    查看更多内容
+                                </div>
+                            </div>
+
+                        </div>)
+                })}
+            </div>
+        </div>
+    )
+}
