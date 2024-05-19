a925adac0ce11bf6e55b7f78c19b7e767fffa52b
Heath <duweihengmr@163.com>
2024-05-19 23:38:46 +0800

    config

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-use serde::de::value;
 use tauri::{AppHandle, Manager, SystemTray, SystemTrayEvent, Wry};
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_websocket::init())
         .system_tray(SystemTray::new())
         .on_system_tray_event(system_tray_event)
         .run(tauri::generate_context!())
