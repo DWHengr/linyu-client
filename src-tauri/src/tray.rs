0a58e4e9b4ec95497b6c207b421b6c86e510eee6
Heath <duweihengmr@163.com>
2024-06-04 21:17:00 +0800

    tray

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -1,26 +1,50 @@
 use tauri::{
-    tray::{ClickType, TrayIconBuilder},
-    Manager, Runtime
+    tray::{MouseButton, TrayIconBuilder, TrayIconEvent},
+    Manager, Runtime,
 };
 
 pub fn create_tray<R: Runtime>(app: &tauri::AppHandle<R>) -> tauri::Result<()> {
-    let _ = TrayIconBuilder::new()
+    let _ = TrayIconBuilder::with_id("tray")
         .tooltip("linyu")
         .icon(app.default_window_icon().unwrap().clone())
-        .on_tray_icon_event(|tray, event| {
-            if event.click_type == ClickType::Left {
-                let windows = tray.app_handle().webview_windows();
-                for (key, value) in windows {
-                    if key == "login" || key == "home" {
-                        value.show().unwrap();
-                        value.unminimize().unwrap();
-                        value.set_focus().unwrap();
+        .on_tray_icon_event(|tray, event| match event {
+            TrayIconEvent::Click {
+                id: _,
+                position,
+                rect: _,
+                button,
+                button_state: _,
+            } => match button {
+                MouseButton::Left {} => {
+                    let windows = tray.app_handle().webview_windows();
+                    for (key, value) in windows {
+                        if key == "login" || key == "home" {
+                            value.show().unwrap();
+                            value.unminimize().unwrap();
+                            value.set_focus().unwrap();
+                        }
                     }
                 }
+                MouseButton::Right {} => {
+                    tray.app_handle().emit("tray_menu", position).unwrap();
+                }
+                _ => {}
+            },
+            TrayIconEvent::Enter {
+                id: _,
+                position,
+                rect: _,
+            } => {
+                tray.app_handle().emit("tray_enter", position).unwrap();
             }
-            if event.click_type == ClickType::Right {
-             tray.app_handle().emit("tray_menu", event.position).unwrap();
+            TrayIconEvent::Leave {
+                id: _,
+                position,
+                rect: _,
+            } => {
+                tray.app_handle().emit("tray_leave", position).unwrap();
             }
+            _ => {}
         })
         .build(app);
     Ok(())
