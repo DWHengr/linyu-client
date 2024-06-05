00fdb8e3b71b925855559a75cb7c3d61e6ecc1c9
Heath <duweihengmr@163.com>
2024-06-05 21:15:02 +0800

    app

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,33 @@ import Home from "./pages/Home/index.jsx";
 import Login from "./pages/Login/index.jsx";
 import TrayMenu from "./pages/TrayMenu/index.jsx";
 import ChatWindow from "./pages/ChatWindow/index.jsx";
+import MessageBox from "./pages/MessageBox/index.jsx";
+import {useEffect} from "react";
 
 function App() {
+
+    const ignoreKey = ["v", "a", "c", "z"];
+
+    const onGlobalKeyDown = (e) => {
+        if ((e.ctrlKey || e.altKey) && ignoreKey.indexOf(e.key) < 0) {
+            e.preventDefault();
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("keydown", onGlobalKeyDown);
+        return () => {
+            window.removeEventListener("keydown", onGlobalKeyDown);
+        };
+    }, []);
+
     return (
-        <div>
+        <div onContextMenu={(e) => e.preventDefault()}>
             <Switch>
                 <Route path="/login" component={Login}></Route>
                 <Route path="/home" component={Home}></Route>
                 <Route path="/tray" component={TrayMenu}></Route>
+                <Route path="/messagebox" component={MessageBox}></Route>
                 <Route path="/chat" component={ChatWindow}></Route>
                 <Redirect path="/" to="/login"/>
             </Switch>
