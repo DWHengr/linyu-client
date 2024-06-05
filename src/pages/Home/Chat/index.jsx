d5da1d4019e27fdd924848d77af29501c907f62d
Heath <duweihengmr@163.com>
2024-06-05 21:14:29 +0800

    chat

--- src/pages/Home/Chat/index.jsx
+++ src/pages/Home/Chat/index.jsx
@@ -14,12 +14,15 @@ import FriendApi from "../../../api/friend.js";
 import {useHistory} from "react-router-dom";
 import FriendSearchCard from "../../../componets/FriendSearchCard/index.jsx";
 import CustomEmpty from "../../../componets/CustomEmpty/index.jsx";
+import {WebviewWindow} from "@tauri-apps/api/WebviewWindow";
 
 export default function Chat() {
     const chatStoreData = useSelector((state) => state.chatData);
     const [selectedChatId, setSelectedChatId] = useState(chatStoreData.currentChatId)
     const currentToId = useRef(chatStoreData.currentChatId)//消息目标用户
     const [selectedUserInfo, setSelectedUserInfo] = useState(chatStoreData.currentChatUserInfo);
+    const homeStoreData = useSelector(store => store.homeData);
+    const currentOption = useRef(homeStoreData.currentOption)
     const selectedRightUserInfo = useRef(null);
     const rightSelected = useRef(null);
     const [menuPosition, setMenuPosition] = useState(null);
@@ -38,19 +41,42 @@ export default function Chat() {
             if (res.code === 0) {
                 setTopChatsData(res.data.tops)
                 setAllChatsData(res.data.others)
+                emit("chat-list", [...res.data.tops, ...res.data.others])
             }
         })
     }
 
     useEffect(() => {
+        setSelectedUserInfo(chatStoreData.currentChatUserInfo)
+    }, [chatStoreData.currentChatUserInfo])
+
+    useEffect(() => {
+        currentOption.current = homeStoreData.currentOption
+    }, [homeStoreData.currentOption])
+    const onRead = (id) => {
+        if (!id || id.length <= 0) return
+        ChatListApi.read(id).then(() => {
+            onGetChatList()
+            emit("on-unread-info", {})
+        })
+    }
+
+    useEffect(() => {
+        //home窗口聚焦
+        const window = WebviewWindow.getByLabel('home')
+        let unFocus = window.listen("tauri://focus", (e) => {
+            onRead(currentToId.current)
+            e.stopPropagation()
+        });
         //监听后端接受到的消息
-        const unReceiveListen = listen('on-receive-msg', (event) => {
+        const unReceiveListen = listen('on-receive-msg', async (event) => {
             let data = event.payload
-            if (currentToId.current === data.fromId) {
-                ChatListApi.read(data.fromId).then(() => {
-                    onGetChatList()
-                    emit("on-unread-info", {})
-                })
+            const window = WebviewWindow.getByLabel('home')
+            let isFocused = await window.isFocused()
+            let isMaximized = await window.isMaximized()
+            //为当前发送的用户，并且home窗口聚集
+            if (currentToId.current === data.fromId && isFocused && isMaximized) {
+                onRead(data.fromId)
             } else {
                 onGetChatList()
             }
@@ -68,6 +94,7 @@ export default function Chat() {
             dispatch(deleteChatWindowUser(event.payload.fromId))
         })
         return async () => {
+            (await unFocus)();
             (await unReceiveListen)();
             (await unSendListen)();
             (await unChatDestroyed)();
@@ -91,14 +118,12 @@ export default function Chat() {
         onGetChatList()
     }, []);
 
-    const chatListRightOptions = [
-        {key: "top", label: "置顶"},
-        {key: "unTop", label: "取消置顶"},
-        // {key: "unNoDisturb", label: "设置免打扰"},
+    const chatListRightOptions = [{key: "top", label: "置顶"}, {
+        key: "unTop",
+        label: "取消置顶"
+    }, // {key: "unNoDisturb", label: "设置免打扰"},
         // {key: "unNoDisturb ", label: "取消免打扰"},
-        {key: "newChatWindow", label: "打开独立窗口"},
-        {key: "deleteChat", label: "从聊天列表中移除"},
-    ]
+        {key: "newChatWindow", label: "打开独立窗口"}, {key: "deleteChat", label: "从聊天列表中移除"},]
 
     const onMenuItemClick = (item) => {
         switch (item.key) {
@@ -160,17 +185,13 @@ export default function Chat() {
             CreateChatWindow(storeUserInfo.fromId, "linyu")
             return
         }
-        if (selectedChatId === data.fromId)
-            return
-        dispatch(setCurrentChatId(data.fromId, data))
+        if (selectedChatId === data.fromId) return
         setSelectedUserInfo(data)
+        dispatch(setCurrentChatId(data.fromId, data))
     }
 
     useEffect(() => {
-        ChatListApi.read(chatStoreData.currentChatId).then(res => {
-            onGetChatList()
-            emit("on-unread-info", {})
-        })
+        onRead(chatStoreData.currentChatId)
     }, [chatStoreData.currentChatId]);
 
     const ChatCard = ({info, onClick, onContextMenu}) => {
@@ -178,62 +199,56 @@ export default function Chat() {
         if (info.fromId === selectedChatId) {
             isSelected = true
         }
-        return (
-            <div
-                className={`chat-card ${isSelected ? "selected" : ""}`}
-                onClick={() => onClick(info)}
-                onContextMenu={(e) => {
-                    e.preventDefault()
-                    if (onContextMenu) onContextMenu(e)
-                }}
-            >
-                <img className="chat-card-portrait" src={info.portrait}
-                     alt={info.portrait}/>
-                <div className="chat-card-content">
-                    <div className="chat-card-content-item">
-                        <div
-                            style={{
-                                fontSize: 14,
-                                fontWeight: 600,
-                                color: `${isSelected ? "#FFF" : "1F1F1F"}`
-                            }}
-                            className="ellipsis"
-                        >
-                            {info.remark ? info.remark : info.name}
-                        </div>
-                        <div style={{
-                            fontSize: 10,
-                            color: `${isSelected ? "#F6F6F6" : "#646464"}`
-                        }}>{formatTime(info.updateTime)}</div>
+        return (<div
+            className={`chat-card ${isSelected ? "selected" : ""}`}
+            onMouseDown={(e) => {
+                onClick(info)
+            }}
+            onContextMenu={(e) => {
+                e.preventDefault()
+                if (onContextMenu) onContextMenu(e)
+            }}
+        >
+            <img className="chat-card-portrait" src={info.portrait}
+                 alt={info.portrait}/>
+            <div className="chat-card-content">
+                <div className="chat-card-content-item">
+                    <div
+                        style={{
+                            fontSize: 14, fontWeight: 600, color: `${isSelected ? "#FFF" : "1F1F1F"}`
+                        }}
+                        className="ellipsis"
+                    >
+                        {info.remark ? info.remark : info.name}
                     </div>
-                    <div className="chat-card-content-item">
-                        <div
-                            style={{fontSize: 12, color: `${isSelected ? "#F6F6F6" : "#646464"}`}}
-                            className="ellipsis"
-                        >
-                            {info.lastMsgContent?.content}
-                        </div>
-                        {
-                            info.unreadNum > 0 && !isSelected ? <div style={{
-                                    width: 18,
-                                    minWidth: 18,
-                                    height: 18,
-                                    borderRadius: 16,
-                                    backgroundColor: "#4C9BFF",
-                                    fontSize: 12,
-                                    display: "flex",
-                                    alignItems: "center",
-                                    justifyContent: "center",
-                                    color: "#fff"
-                                }}>
-                                    {info.unreadNum < 99 ? info.unreadNum : "99+"}
-                                </div> :
-                                <></>
-                        }
+                    <div style={{
+                        fontSize: 10, color: `${isSelected ? "#F6F6F6" : "#646464"}`
+                    }}>{formatTime(info.updateTime)}</div>
+                </div>
+                <div className="chat-card-content-item">
+                    <div
+                        style={{fontSize: 12, color: `${isSelected ? "#F6F6F6" : "#646464"}`}}
+                        className="ellipsis"
+                    >
+                        {info.lastMsgContent?.content}
                     </div>
+                    {info.unreadNum > 0 && !isSelected ? <div style={{
+                        width: 18,
+                        minWidth: 18,
+                        height: 18,
+                        borderRadius: 16,
+                        backgroundColor: "#4C9BFF",
+                        fontSize: 12,
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        color: "#fff"
+                    }}>
+                        {info.unreadNum < 99 ? info.unreadNum : "99+"}
+                    </div> : <></>}
                 </div>
             </div>
-        )
+        </div>)
     }
 
     const onSendMsgClick = (friendId) => {
@@ -246,88 +261,64 @@ export default function Chat() {
         })
     }
 
-    return (
-        <div className="chat">
-            <div className="chat-list">
-                <CustomDragDiv className="chat-list-top">
-                    <label className="chat-list-top-title">聊天列表</label>
-                    <div>
-                        <CustomSearchInput
-                            value={searchInfo}
-                            onChange={(v) => setSearchInfo(v)}
-                        />
-                    </div>
-                </CustomDragDiv>
-                <RightClickMenu
-                    position={menuPosition}
-                    options={chatListRightOptions}
-                    onMenuItemClick={onMenuItemClick}
-                />
-                {!searchInfo ?
-                    <div
-                        className="chat-list-items">
-                        {topChatsData?.length > 0 && <div className="chat-list-items-label">置顶</div>}
-                        {
-                            topChatsData.map(data => {
-                                return (
-                                    <ChatCard
-                                        key={data.id}
-                                        onContextMenu={(e) => {
-                                            rightSelected.current = data.fromId;
-                                            selectedRightUserInfo.current = data;
-                                            setMenuPosition({x: e.clientX, y: e.clientY})
-                                        }}
-                                        info={data}
-                                        onClick={() => onChatListClick(data)}
-                                    />
-                                )
-                            })
-                        }
-                        {allChatsData?.length > 0 && <div className="chat-list-items-label">全部</div>}
-                        {
-                            allChatsData.map(data => {
-                                return (
-                                    <ChatCard
-                                        key={data.id}
-                                        onContextMenu={(e) => {
-                                            rightSelected.current = data.fromId;
-                                            selectedRightUserInfo.current = data;
-                                            setMenuPosition({x: e.clientX, y: e.clientY})
-                                        }}
-                                        info={data}
-                                        onClick={() => onChatListClick(data)}
-                                    />
-                                )
-                            })
-                        }
-                    </div>
-                    : <div className="chat-list-items">
-                        {searchFriendsList.length > 0 ?
-                            <div>
-                                {
-                                    searchFriendsList.map((friend) => {
-                                        return (
-                                            <FriendSearchCard
-                                                info={friend}
-                                                onClick={() => onSendMsgClick(friend.friendId)}
-                                            />
-                                        )
-                                    })
-                                }
-                            </div>
-                            : <CustomEmpty/>
-                        }
-                    </div>
-                }
-            </div>
-            {
-                selectedUserInfo ?
-                    <CommonChatFrame userInfo={selectedUserInfo}/>
-                    :
-                    <CustomDragDiv style={{display: "flex", flex: 1, alignItems: "center", justifyContent: "center"}}>
-                        <img style={{height: 120}} src="/bg.png" alt=""/>
-                    </CustomDragDiv>
-            }
+    return (<div className="chat">
+        <div className="chat-list">
+            <CustomDragDiv className="chat-list-top">
+                <label className="chat-list-top-title">聊天列表</label>
+                <div>
+                    <CustomSearchInput
+                        value={searchInfo}
+                        onChange={(v) => setSearchInfo(v)}
+                    />
+                </div>
+            </CustomDragDiv>
+            <RightClickMenu
+                position={menuPosition}
+                options={chatListRightOptions}
+                onMenuItemClick={onMenuItemClick}
+            />
+            {!searchInfo ? <div
+                className="chat-list-items">
+                {topChatsData?.length > 0 && <div className="chat-list-items-label">置顶</div>}
+                {topChatsData.map(data => {
+                    return (<ChatCard
+                        key={data.id}
+                        onContextMenu={(e) => {
+                            rightSelected.current = data.fromId;
+                            selectedRightUserInfo.current = data;
+                            setMenuPosition({x: e.clientX, y: e.clientY})
+                        }}
+                        info={data}
+                        onClick={() => onChatListClick(data)}
+                    />)
+                })}
+                {allChatsData?.length > 0 && <div className="chat-list-items-label">全部</div>}
+                {allChatsData.map(data => {
+                    return (<ChatCard
+                        key={data.id}
+                        onContextMenu={(e) => {
+                            rightSelected.current = data.fromId;
+                            selectedRightUserInfo.current = data;
+                            setMenuPosition({x: e.clientX, y: e.clientY})
+                        }}
+                        info={data}
+                        onClick={() => onChatListClick(data)}
+                    />)
+                })}
+            </div> : <div className="chat-list-items">
+                {searchFriendsList.length > 0 ? <div>
+                    {searchFriendsList.map((friend) => {
+                        return (<FriendSearchCard
+                            info={friend}
+                            onClick={() => onSendMsgClick(friend.friendId)}
+                        />)
+                    })}
+                </div> : <CustomEmpty/>}
+            </div>}
         </div>
-    )
+        {selectedUserInfo ? <CommonChatFrame userInfo={selectedUserInfo}/> :
+            <CustomDragDiv style={{display: "flex", flex: 1, alignItems: "center", justifyContent: "center"}}>
+                <img style={{height: 120}} src="/bg.png" alt=""/>
+            </CustomDragDiv>}
+    </div>)
 }
