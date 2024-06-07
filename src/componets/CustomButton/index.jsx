da24b5486226959fe138e88b8f4350b75f91cbf5
Heath <duweihengmr@163.com>
2024-06-07 21:01:12 +0800

    CustomButton

--- src/componets/CustomButton/index.jsx
+++ src/componets/CustomButton/index.jsx
@@ -1,13 +1,13 @@
 import "./index.css"
 
-export default function CustomButton({children, onClick, width, type, style}) {
+export default function CustomButton({children, onClick, width, type, style, disabled = false}) {
     return (
         <>
             <div
                 style={{width: width, ...style}}
-                className={`custom-button ${type}`}
+                className={`custom-button ${type} ${disabled ? "disabled" : ""}`}
                 onClick={() => {
-                    if (onClick) onClick()
+                    if (onClick && !disabled) onClick()
                 }}
             >
                 {children}
