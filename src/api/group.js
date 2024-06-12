fdf476e2f6ba2cf775e693d799900be18ad71bb8
Heath <duweihengmr@163.com>
2024-06-12 22:20:27 +0800

    group

--- src/api/group.js
+++ src/api/group.js
@@ -9,5 +9,8 @@ export default {
     },
     delete(param) {
         return Http.post("/v1/api/group/delete", param);
-    }
+    },
+    list() {
+        return Http.get("/v1/api/group/list");
+    },
 };
