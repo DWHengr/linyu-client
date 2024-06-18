a8f2ff44683150c0b5918c8b096355153502cabe
Heath <duweihengmr@163.com>
2024-06-18 21:54:44 +0800

    CustomInput

--- src/componets/CustomInput/index.jsx
+++ src/componets/CustomInput/index.jsx
@@ -1,28 +1,35 @@
 import "./index.less"
 import {useState} from "react";
 
-export default function CustomInput({value, onChange, placeholder}) {
+export default function CustomInput({value, onChange, placeholder, pre = "", pos = "", limit = -1, type = "input"}) {
     const [inputValue, setInputValue] = useState(value)
+
     const onCleanValue = () => {
         setInputValue("")
-        onChange("")
+        if (onChange)
+            onChange("")
     }
 
     return (
         <div className="custom-input">
-            <div className="placeholder"></div>
+            {pre && <div className="pre">
+                {pre}
+            </div>}
             <input
                 placeholder={placeholder}
                 value={inputValue}
+                type={type}
                 onChange={(e) => {
-                    setInputValue(e.target.value)
-                    if (onChange) onChange(e.target.value)
+                    if (e.target.value.length <= limit || limit === -1) {
+                        setInputValue(e.target.value)
+                        if (onChange) onChange(e.target.value)
+                    }
                 }}
 
             />
             <div className="operation" style={{display: "flex", justifyContent: "center"}}>
                 <div className={`operation-icon`}/>
-                {inputValue ?
+                {inputValue && type !== "date" ?
                     <i
                         className={`iconfont icon-guanbi operation-icon`}
                         onClick={onCleanValue}
@@ -30,6 +37,9 @@ export default function CustomInput({value, onChange, placeholder}) {
                     <div style={{width: 20}}></div>
                 }
             </div>
+            {limit > 0 &&
+                <div className="character-count">{`${inputValue?.length ? inputValue?.length : 0}/${limit}`}</div>}
+            <div>{pos}</div>
         </div>
     )
 }
