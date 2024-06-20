b2357132257584108b4d73467db20e0804139a13
Heath <duweihengmr@163.com>
2024-06-20 21:52:51 +0800

    storage

---
+++ src/utils/storage.js
@@ -0,0 +1,15 @@
+import {invoke} from "@tauri-apps/api/core";
+
+export async function setItem(key, value) {
+    let userinfo = await invoke("get_user_info", {})
+    let userKey = "user" + userinfo.user_id
+    let data = JSON.parse(localStorage.getItem(userKey))
+    localStorage.setItem(userKey, JSON.stringify({...data, [key]: value}))
+}
+
+export async function getItem(key) {
+    let userinfo = await invoke("get_user_info", {})
+    let userKey = "user" + userinfo.user_id
+    let data = JSON.parse(localStorage.getItem(userKey))
+    return data[key]
+}
