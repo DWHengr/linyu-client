fb8a58de5919ac0bd5164b8efe21d4ceaef1ac68
Heath <duweihengmr@163.com>
2024-06-22 12:16:35 +0800

    message

--- src/api/message.js
+++ src/api/message.js
@@ -7,4 +7,10 @@ export default {
     record(param) {
         return Http.post("/v1/api/message/record", param);
     },
+    sendFile(param, progressHandler) {
+        return Http.uploadFile("/v1/api/message/send/file", param, progressHandler)
+    },
+    getFile(param, progressHandler) {
+        return Http.downloadFile("/v1/api/message/get/file", param, progressHandler)
+    }
 };
