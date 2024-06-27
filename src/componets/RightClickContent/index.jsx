ed5887783b0808e43e4d7070d8c32f2afeece1fb
Heath <duweihengmr@163.com>
2024-06-27 23:17:04 +0800

    RightClickContent

---
+++ src/componets/RightClickContent/index.jsx
@@ -0,0 +1,61 @@
+import "./index.less"
+import {useEffect, useRef, useState} from "react";
+import {listen} from "@tauri-apps/api/event";
+
+export default function RightClickContent({position, visible, children}) {
+    const [contentVisible, setContentVisible] = useState();
+    const [menuPosition, setMenuPosition] = useState({x: 0, y: 0});
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        if (!position) return
+        setContentVisible(true);
+    }, [position])
+
+    useEffect(() => {
+        setContentVisible(visible?.value);
+    }, [visible])
+
+    useEffect(() => {
+        const unListen = listen('drag-click', (event) => {
+            setContentVisible(false)
+        });
+        return async () => {
+            (await unListen)()
+        }
+    }, [])
+
+    useEffect(() => {
+        if (contentVisible) {
+            if (position.y + menuRef.current.clientHeight > window.innerHeight - 20) {
+                position.y = position.y - menuRef.current.clientHeight
+            }
+            if (position.x + menuRef.current.clientWidth > window.innerWidth - 20) {
+                position.x = position.x - menuRef.current.clientWidth
+            }
+            setMenuPosition(position);
+            menuRef.current.focus()
+        }
+    }, [contentVisible])
+
+    return (
+        <div className="right-click-menu">
+            {contentVisible && (
+                <div className="overlay">
+                    <div
+                        ref={menuRef}
+                        style={{
+                            top: menuPosition.y,
+                            left: menuPosition.x,
+                        }}
+                        className="content"
+                        tabIndex="0"
+                        onBlur={() => setContentVisible(false)}
+                    >
+                        {children}
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
--- src/componets/RightClickMenu/index.jsx
+++ src/componets/RightClickMenu/index.jsx
@@ -1,26 +1,13 @@
 import "./index.less"
 import {useEffect, useRef, useState} from "react";
 import {listen} from "@tauri-apps/api/event";
+import RightClickContent from "../RightClickContent/index.jsx";
 
 export default function RightClickMenu({position, options, visible = false, onMenuItemClick, filter = []}) {
-    const [menuVisible, setMenuVisible] = useState(visible);
+    const [menuVisible, setMenuVisible] = useState();
     const [menuPosition, setMenuPosition] = useState({x: 0, y: 0});
     const menuRef = useRef(null);
     const [menuOptions, setMenuOption] = useState(options)
-
-    useEffect(() => {
-        if (!position) return
-        setMenuVisible(true);
-    }, [position])
-
-    useEffect(() => {
-        const unListen = listen('drag-click', (event) => {
-            setMenuVisible(false)
-        });
-        return async () => {
-            (await unListen)()
-        }
-    }, [])
 
     useEffect(() => {
         if (!filter || filter.length <= 0) return
@@ -30,57 +17,29 @@ export default function RightClickMenu({position, options, visible = false, onMe
         setMenuOption(ops)
     }, [filter])
 
-    useEffect(() => {
-        if (menuVisible) {
-            if (position.y + menuRef.current.clientHeight > window.innerHeight - 20) {
-                position.y = position.y - menuRef.current.clientHeight
-            }
-            if (position.x + menuRef.current.clientWidth > window.innerWidth - 20) {
-                position.x = position.x - menuRef.current.clientWidth
-            }
-            setMenuPosition(position);
-            menuRef.current.focus()
-        }
-    }, [menuVisible])
-
-    useEffect(() => {
-        if (menuVisible) setMenuVisible(false);
-    }, [visible])
-
     const onItemClick = (action) => {
-        setMenuVisible(false)
+        setMenuVisible({value: false})
         if (onMenuItemClick) onMenuItemClick(action)
-    };
+    }
 
     return (
-        <div className="right-click-menu">
-            {menuVisible && (
-                <div className="overlay">
-                    <div
-                        ref={menuRef}
-                        style={{
-                            top: menuPosition.y,
-                            left: menuPosition.x,
-                        }}
-                        className="options"
-                        tabIndex="0"
-                        onBlur={() => setMenuVisible(false)}
-                    >
-                        {menuOptions.map((item, index) => {
-                            if (filter.includes(index.key)) {
-                                return <></>
-                            }
-                            return (
-                                <div className="option" key={item.key} onClick={(e) => {
-                                    onItemClick(item)
-                                }}>
-                                    {item.label}
-                                </div>
-                            )
-                        })}
-                    </div>
+        <>
+            <RightClickContent visible={menuVisible} position={position}>
+                <div className="options">
+                    {menuOptions.map((item, index) => {
+                        if (filter.includes(index.key)) {
+                            return <></>
+                        }
+                        return (
+                            <div className="option" key={item.key} onClick={(e) => {
+                                onItemClick(item)
+                            }}>
+                                {item.label}
+                            </div>
+                        )
+                    })}
                 </div>
-            )}
-        </div>
+            </RightClickContent>
+        </>
     )
 }
