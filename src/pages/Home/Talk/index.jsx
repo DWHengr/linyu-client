53b4cf34af774f9079f2a0579b506dbe8a0c3e68
Heath <duweihengmr@163.com>
2024-07-10 22:25:13 +0800

    talk

--- src/pages/Home/Talk/index.jsx
+++ src/pages/Home/Talk/index.jsx
@@ -3,6 +3,7 @@ import {Redirect, Route, Switch} from "react-router-dom";
 import AllTalk from "./AllTalk/index.jsx";
 import DetailTalk from "./DetailTalk/index.jsx";
 import CreateTalk from "./CreateTalk/index.jsx";
+import CustomEmpty from "../../../componets/CustomEmpty/index.jsx";
 
 export default function Talk() {
 
@@ -12,6 +13,7 @@ export default function Talk() {
                 <Route path="/home/talk/all" component={AllTalk}></Route>
                 <Route path="/home/talk/detail" component={DetailTalk}></Route>
                 <Route path="/home/talk/create" component={CreateTalk}></Route>
+                <Route path="/home/talk/reload" component={CustomEmpty}></Route>
                 <Redirect path="/home/talk" to="/home/talk/all"/>
             </Switch>
         </div>
