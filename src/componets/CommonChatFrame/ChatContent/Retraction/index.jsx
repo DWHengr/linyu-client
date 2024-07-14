8f233d942224f8ba0841d4574ea89881e6bbcc9b
Heath <duweihengmr@163.com>
2024-07-14 21:00:39 +0800

    Retraction

---
+++ src/componets/CommonChatFrame/ChatContent/Retraction/index.jsx
@@ -0,0 +1,24 @@
+import "./index.less"
+import {memo} from "react";
+
+const Retraction = memo(({value, right = false, onReedit}) => {
+
+    return (
+        <>
+            <div className="chat-content-retraction">
+                <div>{`${right ? "你" : "对方"}撤回了一条消息`}</div>
+                {right && value?.type === "text" &&
+                    < div
+                        style={{color: "#4C9BFF", marginLeft: 5, cursor: "pointer"}}
+                        onClick={() => {
+                            if (onReedit) onReedit(value)
+                        }}
+                    >
+                        重新编辑
+                    </div>
+                }
+            </div>
+        </>
+    )
+})
+export default Retraction;
