6a1bafe8fc80f01a6052de3e3a04ac693444f142
Heath <duweihengmr@163.com>
2024-07-14 21:01:31 +0800

    Img

--- src/componets/CommonChatFrame/ChatContent/Img/index.jsx
+++ src/componets/CommonChatFrame/ChatContent/Img/index.jsx
@@ -5,28 +5,57 @@ import CreateImageViewer from "../../../../pages/ImageViewer/window.jsx";
 
 const Img = memo(({value, right = false}) => {
     const [imgInfo, setImgInfo] = useState(null)
+    const imgInfoRef = useRef(null)
     const fileInfo = useRef()
+    const [isLoaded, setIsLoaded] = useState(false)
+    const interval = useRef(null)
+    const [retryNum, setRetryNum] = useState(0)
+    const retryNumRef = useRef(0)
 
     useEffect(() => {
         fileInfo.current = JSON.parse(value.msgContent?.content)
         MessageApi.getImg({
             msgId: value.id,
         }).then((res) => {
-            console.log(res)
+            imgInfoRef.current = res?.data
             setImgInfo(res?.data)
         })
+        if (interval.current) clearInterval(interval.current)
+        interval.current = setInterval(() => {
+            if (retryNumRef.current > 60) {
+                clearInterval(interval.current)
+                return
+            }
+            setImgInfo(imgInfoRef.current)
+            retryNumRef.current = retryNumRef.current + 1
+            setRetryNum(retryNumRef.current)
+        }, 1000)
+
     }, [value])
 
     return (
         <>
             <div className={"chat-content-img"}>
                 <div className={`content  ${right ? "right" : ""}`}>
-                    <img
-                        style={{width: "100%", height: "100%", objectFit: "cover"}}
+                    {!isLoaded ? (
+                        <div className="loading-spinner"></div>
+                    ) : null}
+                    {imgInfo && <img
+                        key={retryNum}
+                        style={{
+                            width: "100%",
+                            height: "100%",
+                            objectFit: "cover",
+                            display: `${!isLoaded ? 'none' : ''}`
+                        }}
                         src={imgInfo}
-                        alt=""
+                        onLoad={() => {
+                            clearInterval(interval.current)
+                            setIsLoaded(true)
+                        }}
+                        alt="加载失败"
                         onClick={() => CreateImageViewer(fileInfo.current.name, imgInfo)}
-                    />
+                    />}
                 </div>
             </div>
         </>
