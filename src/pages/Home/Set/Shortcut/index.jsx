3a655b597244c0415d60fac2fb53a3ce565c6154
Heath <duweihengmr@163.com>
2024-07-20 22:06:26 +0800

    Shortcut

--- src/pages/Home/Set/Shortcut/index.jsx
+++ src/pages/Home/Set/Shortcut/index.jsx
@@ -2,8 +2,28 @@ import "./index.less"
 import CustomDragDiv from "../../../../componets/CustomDragDiv/index.jsx";
 import CustomLine from "../../../../componets/CustomLine/index.jsx";
 import CustomShortcutInput from "../../../../componets/CustomShortcutInput/index.jsx";
+import {useEffect, useState} from "react";
+import {getItem, setItem} from "../../../../utils/storage.js";
+import {UpdateShortcutRegister} from "../../../../utils/shortcut.js";
 
 export default function Shortcut() {
+    const [userSets, SetUserSets] = useState({})
+
+    useEffect(() => {
+        getItem("user-sets").then(value => {
+            SetUserSets(value)
+        })
+    }, [])
+
+    const handleOnChange = (key, value) => {
+        SetUserSets(pre => {
+            let newPre = {...pre, [key]: value}
+            UpdateShortcutRegister(pre[key], value, key)
+            setItem("user-sets", newPre)
+            return newPre;
+        })
+    }
+
 
     return (
         <div className="shortcut-set">
@@ -15,22 +35,26 @@ export default function Shortcut() {
                     <div className="set-item-options">
                         <div className="set-item-option">
                             <div>截图</div>
-                            <CustomShortcutInput/>
+                            <CustomShortcutInput value={userSets.screenshot}
+                                                 onChange={(v) => handleOnChange("screenshot", v)}/>
                         </div>
                         <CustomLine width={1}/>
                         <div className="set-item-option">
                             <div>打开未读会话</div>
-                            <CustomShortcutInput/>
+                            <CustomShortcutInput value={userSets.openUnreadMsg}
+                                                 onChange={(v) => handleOnChange("openUnreadMsg", v)}/>
                         </div>
                         <CustomLine width={1}/>
                         <div className="set-item-option">
                             <div>隐藏/显示主面板</div>
-                            <CustomShortcutInput/>
+                            <CustomShortcutInput value={userSets.hideOrShowHome}
+                                                 onChange={(v) => handleOnChange("hideOrShowHome", v)}/>
                         </div>
                         <CustomLine width={1}/>
                         <div className="set-item-option">
                             <div>关闭会话窗口</div>
-                            <CustomShortcutInput/>
+                            <CustomShortcutInput value={userSets.closeMsgWindow}
+                                                 onChange={(v) => handleOnChange("closeMsgWindow", v)}/>
                         </div>
                     </div>
                 </div>
